f86cf4811252d7434fdd2291b3d1f66f16121253
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-25 21:46:20 +0300

    learnt parameterized function

--- constructor2.cpp
+++ constructor2.cpp
@@ -1,6 +1,6 @@
 //Constructor is a special function that runs automatically when an object is created.its used to initialize values
 //simple constructor
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 class Student{
     public:
@@ -14,5 +14,24 @@ class Student{
 int main(){
     Student s1;
     cout << s1.name;
+    return 0;
+}*/
+
+//parameterized constructor
+#include <iostream>
+using namespace std;
+class Person{
+    public:
+    string name;
+    int age;
+    
+    Person(string n, int a){
+        name = n;
+        age = a;
+    }
+};
+int main(){
+    Person s1("Victoria", 22);
+    cout<< s1.name << " " << s1.age;
     return 0;
 }
