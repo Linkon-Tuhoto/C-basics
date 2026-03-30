5ddb099372cce8a6471cb756ab32b619d6624856
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-30 08:01:25 +0300

    recap on constructors and inheritance

--- recap.cpp
+++ recap.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 class Employee{
     string name;
@@ -25,5 +25,38 @@ int main(){
     e.display();
     Employee f("Kange'the", 50000);
     f.display();
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+class Person{
+    public:
+    string name;
+    Person(string);
+    void displayName();
+};
+class Employee : public Person {
+    int salary;
+    public:
+    Employee(int, string);
+    void displaySalary();
+};
+Employee :: Employee(int s, string n): Person(n){
+    salary = s;
+}
+Person :: Person(string n){
+    name = n;
+}
+void Employee :: displaySalary(){
+    cout << "My name is " << name << " and I earn ksh " << salary << endl;
+}
+void Person :: displayName(){
+    cout << "My name is " << name << endl;
+}
+int main (){
+    Employee g(100000, "Kang'ethe");
+    g.displayName();
+    g.displaySalary();
     return 0;
 }
