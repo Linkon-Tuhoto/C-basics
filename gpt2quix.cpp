bbaffecee842adb25c9f3b4621ebc632c28a97f3
Linkon-Tuhoto <linkonngere@gmail.com>
2026-04-03 15:57:37 +0300

    Inheritance

--- gpt2quix.cpp
+++ gpt2quix.cpp
@@ -40,7 +40,7 @@ int main(){
 
 //Constructor Overloading
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 class Employee{
     public:
@@ -61,5 +61,40 @@ int main(){
     cout << "My name is " << c.name << endl;
     Employee d("King'ori", 22);
     cout << "My name is " << d.name << " and I am " << d.age << " years old." << endl;
+    return 0;
+}*/
+
+//Inheritance Example
+#include <iostream>
+using namespace std;
+class Person{
+    public:
+    string name;
+    Person(string n);
+    void named();
+};
+class Student : public Person{
+    public:
+    int age;
+    Student(string n, int a);
+    void aged();
+};
+Person :: Person(string n){
+    name = n;
+}
+Student :: Student(string n, int a) : Person(n){
+    age = a;
+}
+void Student :: aged(){
+    cout << "My name is " << name << " and I am " << age << "years old." << endl;
+}
+void Person :: named(){
+    cout << "My name is " << name << endl;
+}
+int main(){
+    Person e("King'osh");
+    e.named();
+    Student f("Mathenge", 25);
+    f.aged();
     return 0;
 }
